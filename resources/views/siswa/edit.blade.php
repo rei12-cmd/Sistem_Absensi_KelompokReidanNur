059e8e6b78754b3314f7fcb5b83831aad11d140c
rei12-cmd <2206025@itg.ac.id>
2025-10-15 14:09:09 +0700

    update edit siswa

--- resources/views/siswa/edit.blade.php
+++ resources/views/siswa/edit.blade.php
@@ -17,54 +17,58 @@
 
             <div class="mb-3">
                 <label>Nama Siswa</label>
-                <input type="text" name="nama" class="form-control" value="{{ old('nama') }}">
+                <input type="text" name="nama" class="form-control" value="{{ old('nama', $siswa->nama) }}">
                 @error('nama')<small class="text-danger">{{ $message }}</small>@enderror
             </div>
 
             <div class="mb-3">
                 <label>Username</label>
-                <input type="username" name="username" class="form-control" value="{{ old('username') }}">
+                <input type="username" name="username" class="form-control" value="{{ old('username', default: $siswa->username) }}">
                 @error('username')<small class="text-danger">{{ $message }}</small>@enderror
             </div>
 
             <div class="mb-3">
                 <label>Email</label>
-                <input type="email" name="email" class="form-control" value="{{ old('email') }}">
+                <input type="email" name="email" class="form-control" value="{{ old('email', default:$siswa->email) }}">
                 @error('email')<small class="text-danger">{{ $message }}</small>@enderror
             </div>
 
-            @error('kelas_id')
-            <span class="badge bg-danger">{{ $message }}</span>
-            @enderror
-            <div class="mb-3">
-                <label for="kelas_id" class="form-label">kelas</label>
-                <select class="form-control" id="kelas_id" name="kelas_id">
-                    <option value="">Pilih kelas</option>
-                    @foreach($kelass as $kelas)
-                        <option value="{{ $kelas->id }}">{{ $kelas->nama }}</option>
-                    @endforeach
-                </select>
+
             </div>
+             @error('kelas_id')
+                        <span class="badge bg-danger">{{ $message }}</span>
+                        @enderror
+                        <div class="mb-3">
+                            <label for="kelas_id" class="form-label">Kelas</label>
+                            <select class="form-control" id="kelas_id" name="kelas_id">
+                                <option value="">Pilih Kelas</option>
+                                @foreach($kelass as $k)
+                                    <option value="{{ $k->id }}" {{ old('kelas_id', $k->kelas_id) == $k->id ? 'selected' : '' }}>
+                                        {{ $k->nama }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
 
             <div class="mb-3">
                 <label>Password</label>
-                <input type="password" name="password" class="form-control">
+                <input type="password" name="password" class="form-control"> value="{{ old('password', $siswa->password) }}">
                 @error('password')<small class="text-danger">{{ $message }}</small>@enderror
             </div>
 
             <div class="mb-3">
                 <label>Konfirmasi Password</label>
-                <input type="password" name="password_confirmation" class="form-control">
+                <input type="password" name="password_confirmation" class="form-control"> value="{{ old('password', $siswa->password) }}">
             </div>
 
             <div class="mb-3">
                 <label>Tanggal Lahir</label>
-                <input type="date" name="tanggal_lahir" class="form-control" value="{{ old('tanggal_lahir') }}">
+                <input type="date" name="tanggal_lahir" class="form-control" value="{{ old('tanggal_lahir', default: $siswa->tanggal_lahir ) }}">
             </div>
 
             <div class="mb-3">
                 <label>Alamat</label>
-                <input type="text" name="alamat" class="form-control" value="{{ old('alamat') }}">
+                <input type="text" name="alamat" class="form-control" value="{{ old('alamat', default: $siswa->alamat) }}">
             </div>
 
             <button class="btn btn-primary">Edit</button>
